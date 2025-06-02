72de00aaa567b06d712f9061175b3f08da44ec59
nicso <le.nicso@gmail.com>
2025-06-02 17:50:45 +0200

    better solution

--- player_movements.gd
+++ player_movements.gd
@@ -2,74 +2,91 @@ extends Node2D
 
 @onready var anchor: Node2D = %Anchor
 @onready var ray: RayCast2D = %Ray
-@onready var line: Line = %Line2D
+@onready var line: Line2D = %Line2D
 
 var velocity := Vector2.ZERO
-var gravity := Vector2(0, 4980)
-var rope_length :=400.0
+var gravity := Vector2(0, 980)
+var rope_length_max := 600.0
 var damping := 0.98
+var rope_elasticity := 1280
 
-var anchor_points := [(0.0)]
+var anchor_points := [Vector2(0,0),Vector2(0,0)]
 var collider
 @onready var debug: Control = %Debug
 
+func _physics_process(delta: float) -> void:
+	if anchor_points.size() > line.points.size():
+		line.add_point(Vector2.ZERO)
+	if line.points.size() > anchor_points.size():
+		line.remove_point(0)
+	assign_start_and_end()
+	draw_segments()
+	detect_collision_on_segments()
+	
+	if calculate_rope_total_length() > rope_length_max:
+		global_position -= (global_position - anchor_points[anchor_points.size()-2]).normalized() * rope_elasticity * delta
+	if calculate_rope_total_length() < rope_length_max:
+		global_position += (global_position - anchor_points[anchor_points.size()-2]).normalized() * rope_elasticity * delta
+	global_position += gravity * delta 
+	
+
+func calculate_rope_total_length() -> int:
+	var length := 0.0
+	for i in len(anchor_points):
+		if i < anchor_points.size() - 1:
+			length += anchor_points[i].distance_to(anchor_points[i+1])
+	return length
+
+func assign_start_and_end():
+	anchor_points[0] = anchor.global_position 
+	anchor_points[anchor_points.size() - 1] = global_position
+	
+
+func distribute_segments():
+	pass
+
+func add_segment(point:Vector2):
+	anchor_points.append(point)
+	anchor_points[anchor_points.size() - 2] = point
+	pass
+
+func detect_collision_on_segments():
+	for i in len(anchor_points):
+		if i > 0:
+			#var a = anchor_points[i-1] - anchor_points[i] as Vector2
+			var ray_param = PhysicsRayQueryParameters2D.new()
+			ray_param.from = anchor_points[i-1]
+			ray_param.to = anchor_points[i]
+			ray_param.collide_with_areas = true
+			var space_state = get_world_2d().direct_space_state
+			var result = space_state.intersect_ray(ray_param) 
+			if result:
+				var exist := false
+				for point: Vector2 in anchor_points:
+					if point.distance_squared_to(result.get("position")) < 0.2:
+						exist = true
+				if not exist:
+					add_segment(result.get("position"))
+				
+			
+	pass
+
+	
+func draw_segments():
+	for i in len(anchor_points):
+		line.set_point_position(i, anchor_points[i])
+
 
 func _process(delta: float) -> void:
-	if anchor == null:
-		pass
-		
-	anchor_points[0] = anchor.global_position
-	velocity += gravity * delta
-	position += velocity * delta
-
-	var to_node = position - anchor_points.back()
-	var direction = to_node.normalized()
-	var current_length = to_node.length()
-
-	if current_length != 0:
-		position = anchor_points.back() + direction * rope_length
-		var tangent = Vector2(-direction.y, direction.x)
-		var tangential_velocity = velocity.project(tangent)
-		var radial_velocity = velocity.project(direction)
-		var dot = velocity.normalized().dot(direction)
-		radial_velocity *= 1.0 - clamp(abs(dot), 0.0, 1.0) * 0.2  # 0.3 = force du frein
-		velocity = tangential_velocity + radial_velocity
-		
-
-		velocity *= damping
-		
-		# cast for obstacles
-
-		ray.target_position = anchor_points.back() - global_position
-		
-		if ray.is_colliding(): 
-			if ray.get_collision_point().y != anchor_points.back()[1]:
-				anchor_points.append(ray.get_collision_point())
-				var col = ray.get_collision_point()
-		elif anchor_points.size() > 1:
-			anchor_points.pop_back()
-		if anchor_points.size() > 1:
-			var last_segment := ray.get_collision_point() - global_position
-			var previous_segment =  anchor_points[anchor_points.size() - 2] - anchor_points[anchor_points.size() - 1]
-			var dot_ = last_segment.normalized().dot(previous_segment.normalized())
-			var dbg = debug.get_child(0) as Label
-			dbg.text =  str(dot_)
-			print(dot_ )
-			
-			if dot_ < 0:
-				anchor_points.pop_back()
-		
-		# draw rope
-		if anchor:
-			line.target1 = self
-			line.target2 = anchor
-		
-		if Input.is_action_pressed("game_move_up"):
-			rope_length -= delta * 130.0
-		if Input.is_action_pressed("game_move_down"):
-			rope_length += delta * 130.0
-		
-		if Input.is_action_pressed("game_shoot_left"):
-			position.x += delta * 900.0
-		if Input.is_action_pressed("game_shoot_right"):
-			position.x -= delta * 900.0
+	handle_player_controls(delta)
+	
+func handle_player_controls(delta):
+	if Input.is_action_pressed("game_move_up"):
+		rope_length_max -= delta * 130.0
+	if Input.is_action_pressed("game_move_down"):
+		rope_length_max += delta * 130.0
+	
+	if Input.is_action_pressed("game_shoot_left"):
+		position.x += delta * 900.0
+	if Input.is_action_pressed("game_shoot_right"):
+		position.x -= delta * 900.0
